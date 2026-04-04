fd848342ae391ed6c1e1d1a3eef8b6c76a27b685
Koen van Gasteren <gasteren@gmail.com>
2026-04-04 11:31:24 +0200

    update my account page

--- app/account/[[...rest]]/page.tsx
+++ app/account/[[...rest]]/page.tsx
@@ -1,20 +1,20 @@
 import { UserProfile } from "@clerk/nextjs";
-import PageHeader from "@/components/PageHeader";
 
 export default function AccountPage() {
   return (
     <div className="space-y-6">
-      <PageHeader title="My account" />
       <UserProfile
         path="/account"
         appearance={{
           elements: {
             rootBox: "w-full",
-            card: "shadow-none border border-neutral-200 rounded-lg w-full !shadow-none",
+            card: "shadow-none !shadow-none border border-neutral-200 rounded-lg w-full",
             cardBox: "shadow-none !shadow-none",
-            navbar: "border-r border-neutral-200",
+            navbar: "!bg-white border-r border-neutral-200",
             navbarButton: "text-sm",
             pageScrollBox: "p-6",
+            userPreviewAvatarContainer: "hidden",
+            userPreviewTextContainer: "hidden",
           },
         }}
       />
